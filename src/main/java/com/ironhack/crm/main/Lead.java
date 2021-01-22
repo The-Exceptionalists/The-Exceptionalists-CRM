3311534cc7abe4e42077a6162447ef90f9448669
anjenaex-ironhack <anjenaexIronhack@gmail.com>
2021-01-22 23:31:19 +0100

    first commit

---
+++ src/main/java/com/ironhack/crm/main/Lead.java
@@ -0,0 +1,87 @@
+package com.ironhack.crm.main;
+
+public class Lead {
+
+    private static int counter;
+    private int id;
+    private String name;
+    private String email;
+    private String companyName;
+    private String phoneNumber;
+
+    public Lead(int id, String name, String email, String companyName, String phoneNumber) {
+        counter++;
+        setId(counter);
+        setName(name);
+        setEmail(email);
+        setCompanyName(companyName);
+        setPhoneNumber(phoneNumber);
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+    public static void setCounter(int counter) {
+        Lead.counter = counter;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        //"\\b[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b"
+        this.email = email;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+
+        /*@Test
+        public void whenMatchesPhoneNumber_thenCorrect() {
+            String patterns
+                    = "^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$"
+                    + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?){2}\\d{3}$"
+                    + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?)(\\d{2}[ ]?){2}\\d{2}$";
+
+            String[] validPhoneNumbers
+                    = {"2055550125","202 555 0125", "(202) 555-0125", "+111 (202) 555-0125",
+                    "636 856 789", "+111 636 856 789", "636 85 67 89", "+111 636 85 67 89"};
+
+            Pattern pattern = Pattern.compile(patterns);
+            for(String phoneNumber : validPhoneNumbers) {
+                Matcher matcher = pattern.matcher(phoneNumber);
+                assertTrue(matcher.matches());
+            }
+        }*/
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ src/main/java/com/ironhack/crm/main/LeadTest.java
@@ -0,0 +1,24 @@
+package com.ironhack.crm.main;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeadTest {
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void setCompanyName() {
+    }
+
+    @Test
+    void setPhoneNumber() {
+    }
+}
