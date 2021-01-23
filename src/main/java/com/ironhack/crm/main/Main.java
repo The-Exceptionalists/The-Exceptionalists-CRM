7fc321ea51aa9fc011df1a7fd1b00b94bc5a0738
scorsaro <salvatore.corsaro@live.com>
2021-01-23 16:50:54 +0100

    added javaDocs storage class.

--- src/main/java/com/ironhack/crm/main/Main.java
+++ src/main/java/com/ironhack/crm/main/Main.java
@@ -1,20 +1,21 @@
 package com.ironhack.crm.main;
 
-import com.ironhack.crm.classes.Account;
-import com.ironhack.crm.classes.Contact;
-import com.ironhack.crm.enums.Industry;
-import com.ironhack.crm.utilities.Storage;
+
+import com.ironhack.crm.utilities.Buffer;
+import com.ironhack.crm.utilities.Output;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Contact> contactList = new ArrayList<>();
-        contactList.add(new Contact());
-        List<Opportunity> opportunityList = new ArrayList<>();
-        opportunityList.add(new Opportunity());
-        Account account = new Account("dsgg", Industry.PRODUCE, 1, "sda", "adfs", contactList, opportunityList);
+
+//        List<Contact> contactList = new ArrayList<>();
+//        contactList.add(new Contact());
+//        List<Opportunity> opportunityList = new ArrayList<>();
+//        opportunityList.add(new Opportunity());
+//        Account account = new Account("dsgg", Industry.PRODUCE, 1, "sda", "adfs", contactList, opportunityList);
 //        System.out.println(account);
 //        Account account = new Account("Apple", Industry.OTHER, 1, "a","da", null, null);
         Buffer.insertCentralBox();
