d72c092a655f05731f0470228738bac28a0482bc
adrialopezbou <adrialopezbou@hotmail.com>
2021-01-23 17:43:15 +0100

    Intentando hacer merge

--- src/main/java/com/ironhack/crm/utils/Validator.java
+++ src/main/java/com/ironhack/crm/utils/Validator.java
@@ -5,6 +5,15 @@ import java.util.regex.Pattern;
 
 public class Validator {
 
+    public static boolean validateName(String name) {
+        return validate(name, "^[ÁÉÍÓÚA-Z][a-záéíóú]+(\\s+[ÁÉÍÓÚA-Z]?[a-záéíóú]+)*${1,31}");
+    }
+
+    public static boolean validateCompanyName(String name) {
+
+        return name.length() > 0 && name.length() < 31;
+    }
+
     public static boolean validateEmail(String email) {
         Pattern pattern = Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
         Matcher matcher = pattern.matcher(email);
