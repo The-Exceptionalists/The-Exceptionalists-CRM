66649f16dfe81d4703316b175c5a0b58a01e928c
anjenaex-ironhack <anjenaexIronhack@gmail.com>
2021-01-23 16:24:57 +0100

    Storage.getNewId add to lead

--- src/main/java/com/ironhack/crm/main/Main.java
+++ src/main/java/com/ironhack/crm/main/Main.java
@@ -4,6 +4,13 @@ package com.ironhack.crm.main;
 import com.ironhack.crm.utilities.Buffer;
 import com.ironhack.crm.utilities.Output;
 
+import com.ironhack.crm.classes.Account;
+import com.ironhack.crm.classes.Contact;
+import com.ironhack.crm.classes.Opportunity;
+import com.ironhack.crm.enums.Industry;
+import com.ironhack.crm.enums.Product;
+
+import com.ironhack.crm.utilities.Storage;
 
 import java.util.ArrayList;
 import java.util.List;
