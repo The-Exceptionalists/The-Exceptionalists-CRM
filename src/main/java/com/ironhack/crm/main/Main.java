59320e544d60c96dcd047b673c40a64b359cb621
scorsaro <salvatore.corsaro@live.com>
2021-01-23 10:27:27 +0100

    added the signatures for Storage class and getNewId

--- src/main/java/com/ironhack/crm/main/Main.java
+++ src/main/java/com/ironhack/crm/main/Main.java
@@ -4,6 +4,7 @@ import com.ironhack.crm.classes.Account;
 import com.ironhack.crm.classes.Contact;
 import com.ironhack.crm.classes.Opportunity;
 import com.ironhack.crm.enums.Industry;
+import com.ironhack.crm.utilities.Storage;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,8 +14,9 @@ public class Main {
         List<Contact> contactList = new ArrayList<>();
         contactList.add(new Contact());
         List<Opportunity> opportunityList = new ArrayList<>();
-        //opportunityList.add(new Opportunity());
+        opportunityList.add(new Opportunity());
         Account account = new Account("dsgg", Industry.PRODUCE, 1, "sda", "adfs", contactList, opportunityList);
-        System.out.println(account);
+//        System.out.println(account);
+
     }
 }
