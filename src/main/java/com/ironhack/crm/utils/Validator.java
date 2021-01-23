58bf75296e6772a76c5f1a014185c1b9a6ade01d
adrialopezbou <adrialopezbou@hotmail.com>
2021-01-23 17:44:19 +0100

    Intentando hacer merge

--- src/main/java/com/ironhack/crm/utils/Validator.java
+++ src/main/java/com/ironhack/crm/utils/Validator.java
@@ -5,6 +5,13 @@ import java.util.regex.Pattern;
 
 public class Validator {
 
+    public static boolean validate(String arg, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(arg);
+
+        return matcher.matches();
+    }
+
     public static boolean validateName(String name) {
         return validate(name, "^[ÁÉÍÓÚA-Z][a-záéíóú]+(\\s+[ÁÉÍÓÚA-Z]?[a-záéíóú]+)*${1,31}");
     }
@@ -15,15 +22,19 @@ public class Validator {
     }
 
     public static boolean validateEmail(String email) {
-        Pattern pattern = Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
+        return validate(email, "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
     }
 
     public static boolean validatePhoneNumber(String phoneNumber) {
-        Pattern pattern = Pattern.compile("^\\s*(?:\\+?(\\d{1,3}))?([-. (]*(\\d{3})[-. )]*)?((\\d{3})[-. ]*(\\d{2,4})(?:[-.x ]*(\\d+))?)\\s*$");
-        Matcher matcher = pattern.matcher(phoneNumber);
-        return matcher.matches();
+        String regex = "^\\s*(?:\\+?(\\d{1,3}))?([-. (]*(\\d{3})[-. )]*)?((\\d{3})[-. ]*(\\d{2,4})(?:[-.x ]*(\\d+))?)\\s*$";
+
+        /*String regex = "^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$"
+                + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?){2}\\d{3}$"
+                + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?)(\\d{2}[ ]?){2}\\d{2}$";
+
+        validPhoneNumbers= {"2055550125","202 555 0125", "(202) 555-0125", "+111 (202) 555-0125",
+                "636 856 789", "+111 636 856 789", "636 85 67 89", "+111 636 85 67 89"};*/
+        return validate(phoneNumber, regex);
     }
 
     public static boolean validateCommand(String command) {
