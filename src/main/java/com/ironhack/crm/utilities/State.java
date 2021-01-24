b6cd2e2b85c21cb753a8c302a969a8d489c365dd
Jaume <jasato89@gmail.com>
2021-01-24 13:06:39 +0100

    Renamed Json Persistence to State

--- src/main/java/com/ironhack/crm/utilities/State.java
+++ src/main/java/com/ironhack/crm/utilities/State.java
@@ -15,7 +15,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-abstract class Persistence {
+abstract class State {
 
     public static void saveState() {
         writeLeads();
