6b793c975c891131bde61d149b27e4ff40ae4563
Jaume <jasato89@gmail.com>
2021-01-24 12:49:39 +0100

    Pushed JsonPersistence

--- src/main/java/com/ironhack/crm/utilities/JsonPersistence.java
+++ src/main/java/com/ironhack/crm/utilities/JsonPersistence.java
@@ -1,41 +1,106 @@
-package com.ironhack.crm.classes;
+package com.ironhack.crm.utilities;
 
-import com.ironhack.crm.enums.Product;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.ironhack.crm.classes.Account;
+import com.ironhack.crm.classes.Contact;
+import com.ironhack.crm.classes.Lead;
+import com.ironhack.crm.classes.Opportunity;
 
-public class Database {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
-    public static <T> void addToDatabase(T object) {
+public class JsonPersistence {
 
-        if (object instanceof Contact) {
-            writeInDatabase(contactToCsvString((Contact) object));
-        } else if (object instanceof Account) {
-            writeInDatabase(accountToCsvString((Account) object));
-        } else if (object instanceof Lead) {
-            writeInDatabase(leadToCsvString((Lead) object));
-        } else if (object instanceof Opportunity) {
-            writeInDatabase(opportunityToCsvString((Opportunity) object));
+    public static void saveState() {
+        leadsToJson();
+        accountsToJson();
+    }
+
+    public static void restoreState() {
+        leadsFromJson();
+        accountsFromJson();
+
+    }
+
+    private static void leadsToJson() {
+        List<Lead> leads = Storage.getAllLeads();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String path = "src/main/resources/Database/leads.json";
+        try {
+            FileWriter fileWriter = new FileWriter(path);
+            gson.toJson(leads, fileWriter);
+            fileWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
 
-    private static void writeInDatabase(String line) {
+    private static void leadsFromJson() {
+        String path = "src/main/resources/Database/leads.json";
+        Reader reader = null;
+        try {
+            reader = Files.newBufferedReader(Paths.get(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        List<Lead> leads = new Gson().fromJson(reader, new TypeToken<List<Lead>>() {
+        }.getType());
+
+
+        for (Lead lead : leads) {
+            Storage.add(lead);
+        }
+    }
+
+    private static void accountsToJson() {
+        List<Account> accounts = Storage.getAllAccounts();
+        Gson gson;
+        gson = new GsonBuilder().setPrettyPrinting().create();
+        String path = "src/main/resources/Database/accounts.json";
+        try {
+            FileWriter fileWriter = new FileWriter(path);
+            gson.toJson(accounts, fileWriter);
+            fileWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    private static String  opportunityToCsvString(Opportunity opportunity) {
+    private static void accountsFromJson() {
 
-        return String.format("%s, %d, %d, %s\n",
-                opportunity.getProduct(), opportunity.getQuantity(), opportunity.getDecisionMaker().getId(), opportunity.getStatus());
+
+        String path = "src/main/resources/Database/opportunities.json";
+        Reader reader = null;
+        try {
+            reader = Files.newBufferedReader(Paths.get(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        List<Account> accounts = new Gson().fromJson(reader, new TypeToken<List<Account>>() {
+        }.getType());
+
+
+        for (Account account : accounts) {
+            Storage.add(account);
+            for (Opportunity opportunity : account.getOpportunityList()) {
+                Storage.add(opportunity);
+            }
+            for (Contact contact : account.getContactList()) {
+                Storage.add(contact);
+            }
+        }
 
     }
 
-    private static String leadToCsvString(Lead lead) {
-    }
 
-    private static String  accountToCsvString(Account newAccount) {
-    }
-
-    private static String  contactToCsvString(Contact newContact) {
-        
-    }
 }
