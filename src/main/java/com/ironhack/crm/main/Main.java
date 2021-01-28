2b1e31587448d848c66454334f82c1e9c2ce10fa
scorsaro <salvatore.corsaro@live.com>
2021-01-28 22:34:55 +0100

    ok comments storage, buffer, output

--- src/main/java/com/ironhack/crm/main/Main.java
+++ src/main/java/com/ironhack/crm/main/Main.java
@@ -10,46 +10,10 @@ import java.io.BufferedReader;
 
 public class Main {
     public static void main(String[] args) {
-        Buffer.printSize();
         Buffer.initStringsRepository();
         State.restoreState();
-//        CommandManager.printCommandList();
         while (true) {
             CommandManager.introduceCommand();
         }
-
-       /*CommandManager.printCommandList();
-        while(true) {
-            CommandManager.introduceCommand();
-        }*/
-
-
-//
-//
-//        Buffer.initStringsRepository();
-//        Buffer.insertStringIntoRepository(Buffer.getResultItem() + "1", 7);
-//        Buffer.insertStringIntoRepository("* AC0000042", 10);
-//        Buffer.insertStringIntoRepository("* Peppino Impastato", 11);
-//        Buffer.insertStringIntoRepository("* peppino@gmail.com", 12);
-//        Buffer.insertStringIntoRepository("* Google", 13);
-//        Buffer.insertStringIntoRepository("* +34665423345", 14);
-//
-//        Buffer.setUpLayout();
-//
-//        Buffer.insertOptionsAnchors(1);
-//        Buffer.insertStringIntoRepository("Save", 20);
-//
-//        Buffer.insertItemSolo();
-//        Buffer.setPromptLineOne("Ti piace la pasta?");
-//        Buffer.insertCentralPromptPoints(1);
-//
-////        Buffer.insertItemListByThree(6, 1);
-//
-//        Output.printScreen();
-//
-//        //System.out.println(Validator.validateCommand("lookup opportunity"));
-
     }
-
-
 }
